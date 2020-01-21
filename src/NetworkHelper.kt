8d66824d79e9eaadfb12a4dc2dc9c37a7951e079
Dirk Rosenau <d.rosenau@thalia.de>
2020-01-21 10:05:39 +0100

    Resolve ip address on mac correctly.

--- src/NetworkHelper.kt
+++ src/NetworkHelper.kt
@@ -1,18 +1,30 @@
 import java.net.DatagramSocket
 import java.net.InetAddress
+import java.net.InetSocketAddress
+import java.net.Socket
 
 object NetworkHelper {
-    fun resolveIpAdress(): String? {
+    fun determineIpAdress(): String? {
+
+        return if(System.getProperty("os.name").contains("Mac")){
+            determineIpForMac()
+        }
+        else{
+            determineIpForAllOtherSystems()
+        }
+    }
+
+    private fun determineIpForAllOtherSystems(): String? {
         DatagramSocket().use { socket ->
             socket.connect(InetAddress.getByName("8.8.8.8"), 10002)
             return socket.localAddress.hostAddress
         }
-        /*
-        https://stackoverflow.com/questions/9481865/getting-the-ip-address-of-the-current-machine-using-java
-        on mac:
-        Socket socket = new Socket();
-         socket.connect(new InetSocketAddress("google.com", 80));
-         System.out.println(socket.getLocalAddress());
-         */
+    }
+
+    private fun determineIpForMac(): String? {
+        // https://stackoverflow.com/questions/9481865/getting-the-ip-address-of-the-current-machine-using-java
+        val socket = Socket()
+        socket.connect(InetSocketAddress("google.com", 80))
+        return socket.localAddress.hostAddress
     }
 }
